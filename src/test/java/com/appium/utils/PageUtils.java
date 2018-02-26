322fe3d0f7d3a7e2e91af55d805d876f6f58e7d3
jayasremanchari <jayasremanchari.vl@cognizant.com>
2018-02-26 18:08:15 +0530

    Horizontal Swipe Funtionality Implemented

--- src/test/java/com/appium/utils/PageUtils.java
+++ src/test/java/com/appium/utils/PageUtils.java
@@ -2,11 +2,14 @@ package com.appium.utils;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidKeyCode;
+import java.time.Duration;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -54,6 +57,10 @@ public class PageUtils {
 		return selectedCategory;
 
 
+		}
+	 protected MobileElement findElementByText(String text){
+			return (MobileElement) driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text("+text+")"));
+
 
 		}
 
@@ -62,6 +69,19 @@ public class PageUtils {
 			driver.pressKeyCode(AndroidKeyCode.BACK);
 
 		}
+	 protected void swipe(WebElement element){
+			Point point = element.getLocation(); // topCharts - first element present in the scroll list
+			int startY = point.y;
+			int endY = point.y;
 
+			int startX = (int) ((driver.manage().window().getSize().getWidth()) * 0.80);
+			int endX = (int) ((driver.manage().window().getSize().getWidth()) * 0.10);
+
+			TouchAction actions = new TouchAction(driver);
+
+			actions.press(startX, startY).waitAction(Duration.ofSeconds(2)).moveTo(endX, endY).release().perform();
+
+
+	 }
 
 }
