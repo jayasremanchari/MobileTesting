11cf90c76df8f440f4dd4a2d2588c49b2b510283
jayasremanchari <jayasremanchari.vl@cognizant.com>
2018-02-27 12:06:35 +0530

    Code Refactoring-Comments added for the helper functions used.

--- src/test/java/com/appium/utils/ExcelUtils.java
+++ src/test/java/com/appium/utils/ExcelUtils.java
@@ -8,14 +8,28 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ExcelUtils.
+ */
 public class ExcelUtils {
 
+	/** The Excel w sheet. */
 	private static XSSFSheet ExcelWSheet;
 
+	/** The Excel w book. */
 	private static XSSFWorkbook ExcelWBook;
 
+	/** The Cell. */
 	private static XSSFCell Cell;
 
+	/**
+	 * Load excel data.
+	 *
+	 * @param Path the path
+	 * @param SheetName the sheet name
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	public static void loadExcelData(String Path, String SheetName) throws IOException {
 
 		FileInputStream file = new FileInputStream(new File(Path));
@@ -26,6 +40,13 @@ public class ExcelUtils {
 
 	}
 
+	/**
+	 * Gets the cell value.
+	 *
+	 * @param RowNum the row num
+	 * @param ColNum the col num
+	 * @return the cell value
+	 */
 	public static String getCellValue(int RowNum, int ColNum) {
 
 		Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
--- src/test/java/com/appium/utils/PageUtils.java
+++ src/test/java/com/appium/utils/PageUtils.java
@@ -15,27 +15,61 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class PageUtils {
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PageUtils.
+ */
+public class PageUtils  {
 
+	/** The test page. */
+	protected MobileUtils testPage;
+
+	/** The driver. */
 	protected static AndroidDriver driver;
+
+	/**
+	 * Instantiates a new page utils.
+	 *
+	 * @param driver the driver
+	 */
 	public PageUtils(AndroidDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
+		 testPage =  new MobileUtils();
 	}
 
-	 protected void waitUntilVisible(WebElement element) {
+	 /**
+ 	 * Wait until visible.
+ 	 *
+ 	 * @param element the element
+ 	 * The method checks waits until 30 seconds for the provided web element to be visible.
+ 	 */
+ 	protected void waitUntilVisible(WebElement element) {
 			WebDriverWait wait = new WebDriverWait(driver, 30);
 			wait.until(ExpectedConditions.visibilityOf(element));
 
 		}
 
-	 protected void waitUntilVisible(By locator) {
+	 /**
+ 	 * Wait until visible.
+ 	 *
+ 	 * @param locator the locator
+ 	 * The method checks waits until 30 seconds for the provided web element By locator to be visible.
+ 	 */
+ 	protected void waitUntilVisible(By locator) {
 
 	        WebDriverWait wait = new WebDriverWait(driver, 30);
 	        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 	    }
 
-	 protected boolean elementExists(WebElement element){
+	 /**
+ 	 * Element exists.
+ 	 *
+ 	 * @param element the element
+ 	 * @return true, if successful
+ 	 * The method checks if the element exists, returns true if displayed , else false.
+ 	 */
+ 	protected boolean elementExists(WebElement element){
 		 try{
 
 			 if(element.isDisplayed())
@@ -47,7 +81,16 @@ public class PageUtils {
 		return false;
 	 }
 
-	 protected MobileElement scrollToElement(String resource,String element) {
+	 /**
+ 	 * Scroll to element.
+ 	 *
+ 	 * @param resource the resource
+ 	 * @param element the element
+ 	 * @return the mobile element
+ 	 *
+ 	 * The method executes the Appium vertical scroll function within the container until the element is found.
+ 	 */
+ 	protected MobileElement scrollToElement(String resource,String element) {
 
 		 driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		MobileElement selectedCategory =(MobileElement) driver.findElement(MobileBy.AndroidUIAutomator
@@ -58,19 +101,39 @@ public class PageUtils {
 
 
 		}
-	 protected MobileElement findElementByText(String text){
+
+ 	/**
+ 	 * Find element by text.
+ 	 *
+ 	 * @param text the text
+ 	 * @return the mobile element
+ 	 * The method finds the mobile element using the text parameter of the xml.
+ 	 */
+ 	protected MobileElement findElementByText(String text){
 			return (MobileElement) driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().text("+text+")"));
 
 
 		}
 
-	 public static void previousPage() {
+	 /**
+ 	 * Previous page.
+ 	 * Navigates to previous page.
+ 	 */
+ 	public static void previousPage() {
 
 			((AndroidDriver)driver).pressKeyCode(AndroidKeyCode.BACK);
 
 
 		}
-	 protected void swipe(WebElement element){
+
+ 	/**
+ 	 * Swipe.
+ 	 *
+ 	 * @param element the element
+ 	 * The method executes Horizontal wipe functionality with the X and Y co-ordinates of the screen resoulution.
+ 	 * Also effectively works when rotated.
+ 	 */
+ 	protected void swipe(WebElement element){
 			Point point = element.getLocation(); // topCharts - first element present in the scroll list
 			int startY = point.y;
 			int endY = point.y;
